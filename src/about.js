e22bedf635271ccacf831425ee6fc511da959bcc
Roman Gontar <ashenoooone@gmail.com>
2022-03-17 23:16:26 +0400

    feat: new files

---
+++ src/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './about.css';
+import About from './About';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <About />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
