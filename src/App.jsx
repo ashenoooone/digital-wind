a530a705c53d82b597955a66b124f544702b6134
Igor Koneshov <igorkoneshov@mail.ru>
2022-03-17 16:48:39 +0300

    Structure is changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import Header from './components/Header';
 function App() {
   return (
     <>
+      <Header />
+      <Slider />
       <div className='page'>
-        <Header />
         <Benefits />
       </div>
       <Footer />
