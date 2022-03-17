5a4800e108fc6a263b377db905dd5e30ef435060
Roman Gontar <ashenoooone@gmail.com>
2022-03-17 20:29:14 +0400

    feat: section reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Benefits from './components/Benefits';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Reviews from './components/Reviews';
 
 function App() {
   return (
     <>
       <Header />
-      <Slider />
       <div className='page'>
         <Benefits />
+        <Reviews />
       </div>
       <Footer />
     </>
