16212edf1967d5b99aacf98ae1430d90b7892270
Roman Gontar <ashenoooone@gmail.com>
2022-03-18 21:42:55 +0400

    feat: header slide hiding

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import Benefits from "./components/Benefits";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Slider from "./components/Slider";
-import Reviews from "./components/Reviews";
+import Benefits from './components/Benefits';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Slider from './components/Slider';
+import Reviews from './components/Reviews';
+import { Route } from 'react-router-dom';
+import React from 'react';
 
 function App() {
   return (
     <>
       <Header />
       <Slider />
-      <div className="page">
+      <div className='page'>
         <Benefits />
         <Reviews />
       </div>
