1c0d4ca216b3248312754cc3d485a39d5de2a705
Roman Gontar <ashenoooone@gmail.com>
2022-03-18 22:45:43 +0400

    feat: new structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import Benefits from "./Benefits";
-import Reviews from "./Reviews";
-import ReviewsCard from "./ReviewsCard";
+import React from 'react';
+import Benefits from './Benefits/Benefits';
+import Reviews from './Reviews/Reviews';
 
 const Main = () => {
   return (
