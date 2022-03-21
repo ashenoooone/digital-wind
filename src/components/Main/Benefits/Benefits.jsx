d861c2dd201a12e70ea07cc2bb0c28abd80d5c40
Igor Koneshov <igorkoneshov@mail.ru>
2022-03-21 21:33:51 +0300

    fix: footer and update image

--- src/components/Main/Benefits/Benefits.jsx
+++ src/components/Main/Benefits/Benefits.jsx
@@ -1,59 +1,59 @@
-import React from 'react';
-import starImage from '../../../images/star.svg';
-import benefitsImage from '../../../images/onlineClass.jpg';
-import './Benefits.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import starImage from "../../../images/star.svg";
+import benefitsImage from "../../../images/onlineClass.svg";
+import "./Benefits.css";
+import { Link } from "react-router-dom";
 function Benefits(props) {
   return (
-    <section className='benefits'>
-      <p className='benefits__heading'>
+    <section className="benefits">
+      <p className="benefits__heading">
         Актуальные знания от признанных экспертов рынка для новичков и
         практикующих специалистов.
       </p>
-      <div className='benefits__content'>
-        <div className='benefits__container'>
-          <h2 className='benefits__title'>Преимущества</h2>
-          <ul className='benefits__list'>
-            <li className='benefits__list-item'>
+      <div className="benefits__content">
+        <div className="benefits__container">
+          <h2 className="benefits__title">Преимущества</h2>
+          <ul className="benefits__list">
+            <li className="benefits__list-item">
               <img
                 src={starImage}
-                alt='иконка'
-                className='benefits__list-icon'
+                alt="иконка"
+                className="benefits__list-icon"
               />
-              <p className='benefits__list-paragraph'>
+              <p className="benefits__list-paragraph">
                 Изучаете материал на платформе в любое удобное время
               </p>
             </li>
-            <li className='benefits__list-item'>
+            <li className="benefits__list-item">
               <img
                 src={starImage}
-                alt='иконка'
-                className='benefits__list-icon'
+                alt="иконка"
+                className="benefits__list-icon"
               />
-              <p className='benefits__list-paragraph'>
+              <p className="benefits__list-paragraph">
                 Общаетесь с экспертами и единомышленниками в Telegram
               </p>
             </li>
-            <li className='benefits__list-item'>
+            <li className="benefits__list-item">
               <img
                 src={starImage}
-                alt='иконка'
-                className='benefits__list-icon'
+                alt="иконка"
+                className="benefits__list-icon"
               />
-              <p className='benefits__list-paragraph'>
+              <p className="benefits__list-paragraph">
                 Выполняете практические задания, получаете обратную связь и
                 закрепляете знания
               </p>
             </li>
           </ul>
-          <Link to='error' className='benefits__button'>
+          <Link to="error" className="benefits__button">
             Все курсы
           </Link>
         </div>
         <img
           src={benefitsImage}
-          alt='онлайн класс'
-          className='benefits__image'
+          alt="онлайн класс"
+          className="benefits__image"
         />
       </div>
     </section>
