c3b3376d4b944c13f697ff83b7077dd98cb0e3f4
Roman Gontar <ashenoooone@gmail.com>
2022-03-21 20:02:47 +0300

    feat: burger animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import navIcon from "../../images/navIcon.svg";
-import logo from "../../images/logo.svg";
-import "./Header.css";
-import { Link } from "react-router-dom";
-import burger from "../../images/burder-header.svg";
+import React from 'react';
+import navIcon from '../../images/navIcon.svg';
+import logo from '../../images/logo.svg';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import burger from '../../images/burder-header.svg';
 
 const Header = () => {
   const [headerVisibility, setHeaderVisibility] = React.useState(true);
+  const [burgerActive, setBurgerActive] = React.useState(false);
   const [pos, setPos] = React.useState(113);
   React.useEffect(() => {
     const scrollHandler = () => {
@@ -18,36 +19,45 @@ const Header = () => {
         setPos(window.scrollY);
       }
     };
-    window.addEventListener("scroll", scrollHandler);
+    window.addEventListener('scroll', scrollHandler);
     return () => {
-      window.removeEventListener("scroll", scrollHandler);
+      window.removeEventListener('scroll', scrollHandler);
     };
   }, [pos]);
 
   return (
     <header className={headerVisibility ? `header` : `header header_hide`}>
       <div>
-        <Link to="digital-wind">
-          <img src={logo} alt="логотип" className="header__img" />
+        <Link to='digital-wind'>
+          <img src={logo} alt='логотип' className='header__img' />
         </Link>
       </div>
-      <div className="header__nav">
-        <Link to="info" className="header__link">
+      <div className='header__nav'>
+        <Link to='info' className='header__link'>
           О компании
         </Link>
-        <Link to="courses" className="header__link">
+        <Link to='courses' className='header__link'>
           Курсы
         </Link>
       </div>
-      <div className="header__buttons header__buttons-desktop">
-        <img className="header__button" src={navIcon} />
-        <Link to="/login" className="header__button">
+      <div className='header__buttons header__buttons-desktop'>
+        <img className='header__button' alt='кнопка' src={navIcon} />
+        <Link to='/login' className='header__button'>
           Войти
         </Link>
       </div>
-      <div className="header__buttons header__buttons-mobile">
-        <img className="header__button" src={navIcon} />
-        <img src={burger} className="header__burger" />
+      <div className='header__buttons header__buttons-mobile'>
+        <img className='header__button' alt='кнопка' src={navIcon} />
+        <img
+          src={burger}
+          alt='бургер'
+          className={
+            burgerActive
+              ? 'header__burger header__burger_active'
+              : 'header__burger'
+          }
+          onClick={() => setBurgerActive(!burgerActive)}
+        />
       </div>
     </header>
   );
