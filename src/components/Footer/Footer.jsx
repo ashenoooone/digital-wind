5c9e75e3f00f75152c6baf88e2eabea93421f4c0
Roman Gontar <ashenoooone@gmail.com>
2022-03-21 22:25:41 +0300

    fix: footer adaptation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,39 +1,43 @@
-import React from "react";
-import vkLogo from "../../images/vk-logo.svg";
-import instLogo from "../../images/inst-logo.svg";
-import tgLogo from "../../images/tg-logo.svg";
-import justudyLogo from "../../images/logo-footer.svg";
-import "./Footer.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import vkLogo from '../../images/vk-logo.svg';
+import instLogo from '../../images/inst-logo.svg';
+import tgLogo from '../../images/tg-logo.svg';
+import justudyLogo from '../../images/logo-footer.svg';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer__container">
-        <div className="footer__contacts">
-          <img src={justudyLogo} alt="лого" className="footer__logo" />
-          <p className="footer__email">info@juststudy.online</p>
-          <p className="footer__number">+7 450 300 50 05</p>
+    <footer className='footer'>
+      <div className='footer__container'>
+        <div className='footer__contacts'>
+          <img src={justudyLogo} alt='лого' className='footer__logo' />
+          <p className='footer__email'>info@juststudy.online</p>
+          <p className='footer__number'>+7 450 300 50 05</p>
         </div>
-        <div className="footer__social-networks">
-          <a href="#" className="footer__social-network">
-            <img src={vkLogo} alt="логотип вк" />
+        <div className='footer__social-networks'>
+          <a href='#' className='footer__social-network'>
+            <img src={vkLogo} alt='логотип вк' className='footer__image' />
           </a>
-          <a href="#" className="footer__social-network">
-            <img src={tgLogo} alt="логотип телеграма" />
+          <a href='#' className='footer__social-network'>
+            <img
+              src={tgLogo}
+              alt='логотип телеграма'
+              className='footer__image'
+            />
           </a>
         </div>
       </div>
-      <div className="footer__container">
-        <nav className="footer__nav">
-          <Link to="info" className="footer__nav-item">
+      <div className='footer__container'>
+        <nav className='footer__nav'>
+          <Link to='info' className='footer__nav-item'>
             О компании
           </Link>
-          <Link to="courses" className="footer__nav-item">
+          <Link to='courses' className='footer__nav-item'>
             Курсы
           </Link>
         </nav>
-        <p className="footer__author">Сайт разработали CIFRO-VETER-TEAM</p>
+        <p className='footer__author'>Сайт разработали CIFRO-VETER-TEAM</p>
       </div>
     </footer>
   );
