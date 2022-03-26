959ed9813cc31a73f0f23b2503fc7f5476a8bb08
Igor Koneshov <igorkoneshov@mail.ru>
2022-03-26 16:23:12 +0300

    feat: add Acordion and adaptive to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import Main from './components/Main/Main';
-import NotFoundPage from './components/NotFoundPage/NotFoundPage';
-import AboutCreators from './components/AboutCreators/AboutCreators';
-import Registration from './components/Register/Registration';
-import Login from './components/Login/Login';
-import Courses from './components/Courses/Courses';
-import Profile from './components/Profile/Profile';
-import Course from './components/Course/Course';
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Layout from "./components/Layout/Layout";
+import Main from "./components/Main/Main";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+import AboutCreators from "./components/AboutCreators/AboutCreators";
+import Registration from "./components/Register/Registration";
+import Login from "./components/Login/Login";
+import Courses from "./components/Courses/Courses";
+import Profile from "./components/Profile/Profile";
+import Course from "./components/Course/Course";
+import Theory from "./components/Theory/Theory";
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route path='digital-wind' element={<Main />} />
-        <Route path='*' element={<NotFoundPage />} />
-        <Route path='info' element={<AboutCreators />} />
-        <Route path='register' element={<Registration />} />
-        <Route path='login' element={<Login />} />
-        <Route path='courses' element={<Courses />} />
-        <Route path='profile' element={<Profile />} />
-        <Route path='course' element={<Course />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="digital-wind" element={<Main />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="info" element={<AboutCreators />} />
+        <Route path="register" element={<Registration />} />
+        <Route path="login" element={<Login />} />
+        <Route path="courses" element={<Courses />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="course" element={<Course />} />
+        <Route path="theory" element={<Theory />} />
       </Route>
     </Routes>
   );
--- src/components/Main/Reviews/Reviews.jsx
+++ src/components/Main/Reviews/Reviews.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReviewsCard from '../ReviewsCard/ReviewsCard';
-import avatar_1 from '../../../images/review-avatar.svg';
-import avatar_2 from '../../../images/review-avatar-1.svg';
-import './Reviews.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import ReviewsCard from "../ReviewsCard/ReviewsCard";
+import avatar_1 from "../../../images/review-avatar.svg";
+import avatar_2 from "../../../images/review-avatar-1.svg";
+import "./Reviews.css";
+import { Link } from "react-router-dom";
 
 const Reviews = () => {
   return (
-    <section className='reviews'>
-      <h2 className='reviews__title'>Отзывы</h2>
-      <div className='reviews__row'>
+    <section className="reviews">
+      <h2 className="reviews__title">Отзывы</h2>
+      <div className="reviews__row">
         <ReviewsCard
-          author='Роман Черемухин'
+          author="Роман Черемухин"
           avatar={avatar_1}
-          course='Аналитик Big Data'
-          text='Как работать с таблицей, которая весит несколько десятков гигабайт?
+          course="Аналитик Big Data"
+          text="Как работать с таблицей, которая весит несколько десятков гигабайт?
             Это один из многих практических вопросов, на которые отвечает курс.
             Причем я хотел курс, который систематизирует разрозненные знания, и
             их сразу применять на практике. Ожидания полностью оправдались: не
@@ -22,19 +22,19 @@ const Reviews = () => {
             повседневных задач, связанных с анализом Big Data и работой с
             выгрузками из соцсетей и Яндекс.Метрики. Все доступно, доходчиво, на
             примерах, с контролем полученных знаний на каждом этапе и живым
-            общением с коллегами и наставником в Slack.'
-          title='Не было ни одного урока, который нельзя использовать для решения
-            повседневных задач'
+            общением с коллегами и наставником в Slack."
+          title="Не было ни одного урока, который нельзя использовать для решения
+            повседневных задач"
         />
         <ReviewsCard
-          author='Александр Поляков'
+          author="Александр Поляков"
           avatar={avatar_2}
-          course='Основы Python'
-          text='На курс я попал со школьными знаниями Турбо Паскаля и с энтузиазмом изучить основы Python. Успешно применяю в работе. Курс классный, в нем очень много интересного. В качестве введения в язык программирования — идеален.'
-          title='По итогам курса полученные знания я сразу смог применять в работе'
+          course="Основы Python"
+          text="На курс я попал со школьными знаниями Турбо Паскаля и с энтузиазмом изучить основы Python. Успешно применяю в работе. Курс классный, в нем очень много интересного. В качестве введения в язык программирования — идеален."
+          title="По итогам курса полученные знания я сразу смог применять в работе"
         />
       </div>
-      <Link to='error' className='review__link'>
+      <Link to="/theory" className="review__link">
         Все отзывы
       </Link>
     </section>
