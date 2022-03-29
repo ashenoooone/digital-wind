b23ccaf97f000e2c94f9ca22dd341a9c16dbf462
Igor Koneshov <igorkoneshov@mail.ru>
2022-03-29 21:41:33 +0300

    feat: add theory description page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import Main from './components/Main/Main';
-import NotFoundPage from './components/NotFoundPage/NotFoundPage';
-import AboutCreators from './components/AboutCreators/AboutCreators';
-import Registration from './components/Register/Registration';
-import Login from './components/Login/Login';
-import Courses from './components/Courses/Courses';
-import Profile from './components/Profile/Profile';
-import Course from './components/Course/Course';
-import RequierAuth from './hoc/RequierAuth';
-import CourseInner from './components/CourseInner/CourseInner';
-import IsAlreadyAuth from './hoc/IsAlreadyAuth';
-import Theory from './components/Theory/Theory';
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Layout from "./components/Layout/Layout";
+import Main from "./components/Main/Main";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+import AboutCreators from "./components/AboutCreators/AboutCreators";
+import Registration from "./components/Register/Registration";
+import Login from "./components/Login/Login";
+import Courses from "./components/Courses/Courses";
+import Profile from "./components/Profile/Profile";
+import Course from "./components/Course/Course";
+import RequierAuth from "./hoc/RequierAuth";
+import CourseInner from "./components/CourseInner/CourseInner";
+import IsAlreadyAuth from "./hoc/IsAlreadyAuth";
+import Theory from "./components/Theory/Theory";
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route path='digital-wind' element={<Main />} />
-        <Route path='*' element={<NotFoundPage />} />
-        <Route path='info' element={<AboutCreators />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="digital-wind" element={<Main />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="info" element={<AboutCreators />} />
         <Route
-          path='register'
+          path="register"
           element={
             <IsAlreadyAuth>
               <Registration />
@@ -29,30 +29,31 @@ function App() {
           }
         />
         <Route
-          path='/login'
+          path="/login"
           element={
             <IsAlreadyAuth>
               <Login />
             </IsAlreadyAuth>
           }
         />
-        <Route path='courses' element={<Courses />} />
-        <Route
-          path='course-inner'
+        <Route path="courses" element={<Courses />} />
+        {/* <Route
+          path="course-inner"
           element={
-            <CourseInner name='Веб-вёрстка. Базовый уровень' title='Введение' />
+            <CourseInner name="Веб-вёрстка. Базовый уровень" title="Введение" />
           }
-        />
+        /> */}
         <Route
-          path='profile'
+          path="profile"
           element={
             <RequierAuth>
               <Profile />
             </RequierAuth>
           }
         />
-        <Route path='course' element={<Course />} />
-        <Route path='theory' element={<Theory />} />
+        <Route path="course" element={<Course />} />
+        <Route path="theory" element={<Theory />} />
+        <Route path="description" element={<CourseInner />} />
       </Route>
     </Routes>
   );
--- src/components/Theory/TheoryItem/Acordion/Acordion.jsx
+++ src/components/Theory/TheoryItem/Acordion/Acordion.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import Style from "../../Theory.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Acordion = (props) => {
+  const navigate = useNavigate();
+  const goToInformativePage = () =>
+    navigate("/description", {
+      state: {
+        description: props.content,
+        title: props.title,
+        level: props.level,
+      },
+    });
+
   return (
-    <div className={Style["course__info-item"]}>
+    <div className={Style["course__info-item"]} onClick={goToInformativePage}>
       <h2 className={Style["course__info-title"]}>{props.title}</h2>
       <div className={Style["course__info-img"]}></div>
     </div>
