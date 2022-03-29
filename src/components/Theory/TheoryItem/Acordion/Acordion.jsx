8855fbdf6e88e79100716f02f379e4b15b2416d9
Igor Koneshov <igorkoneshov@mail.ru>
2022-03-29 18:55:47 +0300

    fix: delete console.log in Acordion component

--- src/components/Theory/TheoryItem/Acordion/Acordion.jsx
+++ src/components/Theory/TheoryItem/Acordion/Acordion.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Style from "../../Theory.module.css";
 
 const Acordion = (props) => {
-  console.log(props);
   return (
     <div className={Style["course__info-item"]}>
       <h2 className={Style["course__info-title"]}>{props.title}</h2>
