24a79f097c354192265281d52ee42d9569a11f3f
Roman Gontar <ashenoooone@gmail.com>
2022-03-29 23:51:42 +0400

    feat: nav bar in courseinner

--- src/components/CourseInner/CourseInner.jsx
+++ src/components/CourseInner/CourseInner.jsx
@@ -1,14 +1,35 @@
-import React from "react";
-import "./CourseInner.css";
-import { useLocation } from "react-router-dom";
+import React from 'react';
+import './CourseInner.css';
+import { useLocation } from 'react-router-dom';
+import white_burger from '../../images/white-burger.svg';
+import stroke_white from '../../images/stroke-white.svg';
 
 const CourseInner = () => {
   const courseInformation = useLocation().state;
+  const [nav, setNav] = React.useState(false);
   return (
-    <section className="course-inner">
-      <p className="course-inner__name">{courseInformation.level}</p>
-      <h1 className="course-inner__title">{courseInformation.title}</h1>
-      <p className="course-inner__par">{courseInformation.description}</p>
+    <section className='course-inner'>
+      <nav className={`nav ${nav && 'nav-active'}`}>
+        <div className='nav__container'>
+          <img
+            src={nav ? stroke_white : white_burger}
+            alt=''
+            className='nav__img'
+            onClick={() => setNav(!nav)}
+          />
+          <div className={`nav__bar ${!nav && 'nav__bar-inactive'}`}>
+            <h2 className='nav__title'>Лалилох</h2>
+            <ul className='nav__list'>
+              <li className='nav__item'>лляля</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className='course-inner__container'>
+        <p className='course-inner__name'>{courseInformation.level}</p>
+        <h1 className='course-inner__title'>{courseInformation.title}</h1>
+        <p className='course-inner__par'>{courseInformation.description}</p>
+      </div>
     </section>
   );
 };
