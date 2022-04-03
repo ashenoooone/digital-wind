ce992e12bf8fb4a56333fd4238100a52c6679fc1
Roman Gontar <ashenoooone@gmail.com>
2022-04-03 16:34:45 +0400

    fix: alt

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import './Layout.css';
