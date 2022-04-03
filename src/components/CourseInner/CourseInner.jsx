bb0c4cf652b3383b5700fdd96b389371c6bbf714
Roman Gontar <ashenoooone@gmail.com>
2022-04-03 13:30:26 +0400

    feat: pre tag for html

--- src/components/CourseInner/CourseInner.jsx
+++ src/components/CourseInner/CourseInner.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./CourseInner.css";
-import { useParams, useNavigate } from "react-router-dom";
-import white_burger from "../../images/white-burger.svg";
-import stroke_white from "../../images/stroke-white.svg";
-import NavigationItem from "./NavigationItem/NavigationItem";
-import api from "../../utils/Api";
-import { useHeader } from "../../hooks/useHeader";
+import React from 'react';
+import './CourseInner.css';
+import { useParams, useNavigate } from 'react-router-dom';
+import white_burger from '../../images/white-burger.svg';
+import stroke_white from '../../images/stroke-white.svg';
+import NavigationItem from './NavigationItem/NavigationItem';
+import api from '../../utils/Api';
+import { useHeader } from '../../hooks/useHeader';
 
 const CourseInner = () => {
   let { id } = useParams();
@@ -13,19 +13,19 @@ const CourseInner = () => {
   const { courseId } = useParams();
   const navigate = useNavigate();
   const { isHeaderActive } = useHeader();
-  const goToError = () => navigate("/error");
+  const goToError = () => navigate('/error');
   const [data, setData] = React.useState(null);
   const [course, setCourse] = React.useState(null);
   const [active, setActive] = React.useState(true);
   React.useEffect(() => {
     const escapeHandler = (evt) => {
       if (
-        !evt.target.classList.contains("nav") &&
-        !evt.target.classList.contains("nav__img")
+        !evt.target.classList.contains('nav') &&
+        !evt.target.classList.contains('nav__img')
       )
         setNav(false);
     };
-    document.addEventListener("click", escapeHandler);
+    document.addEventListener('click', escapeHandler);
     Promise.all([api.getTheoryContent(id), api.getCourse(courseId)])
       .then(([theoryContent, course]) => {
         if (!theoryContent) setActive(!active);
@@ -35,25 +35,25 @@ const CourseInner = () => {
       })
       .catch(() => goToError());
     return () => {
-      document.removeEventListener("click", escapeHandler);
+      document.removeEventListener('click', escapeHandler);
     };
   }, [level, id]);
   const [nav, setNav] = React.useState(false);
   return (
-    <section className="course-inner">
+    <section className='course-inner'>
       <nav
-        className={`nav ${nav && "nav-active"} ${
-          !isHeaderActive ? "nav-adaptive-header" : ""
+        className={`nav ${nav && 'nav-active'} ${
+          !isHeaderActive ? 'nav-adaptive-header' : ''
         }`}
       >
-        <div className="nav__container">
+        <div className='nav__container'>
           <img
             src={nav ? stroke_white : white_burger}
-            alt=""
-            className="nav__img"
+            alt=''
+            className='nav__img'
             onClick={() => setNav(!nav)}
           />
-          <div className={`nav__bar ${!nav && "nav__bar-inactive"}`}>
+          <div className={`nav__bar ${!nav && 'nav__bar-inactive'}`}>
             {course &&
               course.map((item) => {
                 return item.courseModuleHeaders.map((el) => {
@@ -71,17 +71,17 @@ const CourseInner = () => {
           </div>
         </div>
       </nav>
-      <div className="course-inner__container">
-        <p className="course-inner__name">{level}</p>
-        <h1 className="course-inner__title">{data?.title}</h1>
-        <p className="course-inner__par">{data?.description}</p>
-        <div className="course-inner__buttons">
+      <div className='course-inner__container'>
+        <p className='course-inner__name'>{level}</p>
+        <h1 className='course-inner__title'>{data?.title}</h1>
+        <pre className='course-inner__par'>{data?.description}</pre>
+        <div className='course-inner__buttons'>
           {id != 1 && (
             <button
               onClick={() =>
                 navigate(`/description/${courseId}/${level}/${--id}`)
               }
-              className="course-inner__button button"
+              className='course-inner__button button'
             >
               Назад
             </button>
@@ -91,7 +91,7 @@ const CourseInner = () => {
               onClick={() =>
                 navigate(`/description/${courseId}/${level}/${++id}`)
               }
-              className="course-inner__button button"
+              className='course-inner__button button'
             >
               Далее
             </button>
--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store, { persister } from './components/store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persister}>
           <App />
         </PersistGate>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
